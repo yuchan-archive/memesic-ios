f4f33672584abca06775ce31f49e861f8214b30f
Yusuke Ohashi <yusuke@junkpiano.me>
2014-09-07 23:03:12 +0900

    Initial Commit

---
+++ Memesic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memesic
+//
+//  Created by Ohashi Yusuke on 9/7/14.
+//  Copyright (c) 2014 Ohashi Yusuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Memesic/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Memesic
+//
+//  Created by Ohashi Yusuke on 9/7/14.
+//  Copyright (c) 2014 Ohashi Yusuke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
